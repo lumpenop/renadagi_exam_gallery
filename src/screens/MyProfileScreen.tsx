f427378fdcd5c6cb95021c44851b066afc064826
lumpenop <swk5373@gmail.com>
2023-05-16 01:01:19 +0900

    feat: isLogin 시 페이지 이동 까지

---
+++ src/screens/MyProfileScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const MyProfileScreen = () => {
+  return (
+    <View>
+      <Text>MyProfileScreen hi</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  block: {},
+});
+
+export default MyProfileScreen;
--- src/screens/RootStack.tsx
+++ src/screens/RootStack.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {StyleSheet} from 'react-native';
 import {useUserContext} from 'src/contexts/UserContext';
+import {subscribeAuth} from 'src/lib/auth';
+import {getUser} from 'src/lib/users';
 import MainTab from 'src/screens/MainTab';
 import SignInScreen from 'src/screens/SignInScreen';
 import WelcomeScreen from 'src/screens/WelcomeScreen';
@@ -9,7 +11,26 @@ import WelcomeScreen from 'src/screens/WelcomeScreen';
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
-  const {user} = useUserContext();
+  const {user, setUser} = useUserContext();
+
+  React.useEffect(() => {
+    // 컴포넌트 첫 로딩 시 로그인 상태를 확인하고 UserContext에 적용
+    const unsubscribe = subscribeAuth(async currentUser => {
+      // 여기에 등록한 함수는 사용자 정보가 바뀔 때마다 호출되는데
+      // 처음 호출될 때 바로 unsubscribe해 한 번 호출된 후에는 더 이상 호출되지 않게 설정
+
+      unsubscribe();
+      if (!currentUser) {
+        return;
+      }
+      const profile = await getUser(currentUser.uid);
+      if (!profile) {
+        return;
+      }
+      setUser(profile);
+    });
+  }, [setUser]);
+
   return (
     <Stack.Navigator>
       {user ? (
